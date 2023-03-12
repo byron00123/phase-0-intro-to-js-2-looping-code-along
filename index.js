f8d862c0af59a2e8fd78e5902e520c71575ef0e4
Byron Omondi <omondibyron25@gmail.com>
2023-03-12 22:13:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+  let messagesArray = [];
+
+  for (let i = 0; i < namesArray.length; i++) {
+    messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messagesArray;
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+
